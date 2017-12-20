e7d98b31d74384ce6c447a293fa05509e6fba69b
techtat95 <otatman95@gmail.com>
2017-12-19 21:21:19 -0500

    Create Info.java

---
+++ Info.java
@@ -0,0 +1,26 @@
+
+	public interface Info {
+
+	    public static final int BOARD_WIDTH = 1600;
+	    public static final int BOARD_HEIGHT = 900;
+	    public static final int GROUND = 810;
+	    public static final int BOMB_HEIGHT = 5;
+	    public static final int ALIEN_HEIGHT = 12;
+	    public static final int ALIEN_WIDTH = 12;
+	    public static final int BORDER_RIGHT = 30;
+	    public static final int BORDER_LEFT = 5;
+	    public static final int GO_DOWN = 100;
+	    public static final int NUMBER_OF_ALIENS_TO_DESTROY = 24;
+	    public static final int CHANCE = 5;
+	    public static final int DELAY = 17;
+	    public static final int SHOT_WIDTH = 17;
+	    public static final int SHOT_HEIGHT = 17;
+	    public static final int DEFENDER_WIDTH = 15;
+	    public static final int DEFENDER_HEIGHT = 10;
+	    public static final int SHIP_HEIGHT = 50;
+	    public static final int SHIP_WIDTH = 75; 
+	    public final int START_Y_DEFENDER = 520;
+	    public final int START_X_DEFENDER = 400;
+	    public final int GAME_LEFT = 160;
+	    public final int GAME_RIGHT = 1440;
+	}
